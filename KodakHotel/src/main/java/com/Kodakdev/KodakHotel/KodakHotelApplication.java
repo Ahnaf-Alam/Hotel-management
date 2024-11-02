f0dd066188b89e66f501f2190dbc691f048d42d9
Ahnaf Alam <ahnafalam15@gmail.com>
2024-11-03 00:02:01 +0600

    add required dependency

---
+++ KodakHotel/src/main/java/com/Kodakdev/KodakHotel/KodakHotelApplication.java
@@ -0,0 +1,13 @@
+package com.Kodakdev.KodakHotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KodakHotelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KodakHotelApplication.class, args);
+	}
+
+}
---
+++ KodakHotel/src/test/java/com/Kodakdev/KodakHotel/KodakHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Kodakdev.KodakHotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KodakHotelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
