0d252de1102585f3ddd9bff4f30668e06e4ba4bc
Ahnaf Alam <ahnafalam15@gmail.com>
2025-04-06 23:53:26 +0600

    create home page

--- Kodak_Hotel_FE/src/app/app.config.ts
+++ Kodak_Hotel_FE/src/app/app.config.ts
@@ -1,15 +1,17 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { authInterceptor } from './auth/auth.interceptor';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes), 
     provideClientHydration(), 
     provideHttpClient(withInterceptors([authInterceptor])),
+    importProvidersFrom(BrowserAnimationsModule)
   ]
 };
--- Kodak_Hotel_FE/src/app/home/home.component.ts
+++ Kodak_Hotel_FE/src/app/home/home.component.ts
@@ -1,22 +1,46 @@
 import { Component, inject } from '@angular/core';
 import { AuthService } from '../auth/auth.service';
 import { CommonModule } from '@angular/common';
+import { CalendarModule } from 'primeng/calendar';
+import { FormsModule } from '@angular/forms';
+import { DropdownModule } from 'primeng/dropdown';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, CalendarModule, FormsModule, DropdownModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
   authService = inject(AuthService);
   user?: any;
+  checkIndate?: Date;
+  checkOutDate?: Date;
+  currentDate?: Date;
+  checkOutStartDate?: Date;
+  roomTypes: string[] = [];
+  selectedRoomType?: string;
 
   constructor() {
       this.authService.getCurrentAuthUser().subscribe(response => {
         console.log(response);
         this.user = response;
-      })
+      });
+
+      this.currentDate = new Date();
+
+      // this is testing pupose. We will update when we add apis
+      this.roomTypes.push("Couple");
+      this.roomTypes.push("Single");
+      this.roomTypes.push("Family suit");
+      this.roomTypes.push("President suit");
+  }
+
+  selectedDate() {
+    if(this.checkIndate !== undefined) {
+      this.checkOutStartDate = new Date();
+      this.checkOutStartDate.setDate(this.checkIndate?.getDate() + 1);
+    }
   }
 }
